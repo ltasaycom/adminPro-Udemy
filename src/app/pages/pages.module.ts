9a21087bf885b8e9b5bb35f459cdd8f89cffa515
Luis Miguel Tasayco Márquez <luis.tasayco@textildelvalle.pe>
2020-06-14 19:46:31 -0500

    Input, output y viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
+// ng2-charts
+// import { ChartsModule } from 'ng2-charts';
+
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
 
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 @NgModule({
   declarations: [DashboardComponent,
                  ProgressComponent,
                  Graficas1Component,
-                 PagesComponent],
+                 PagesComponent,
+                 IncrementadorComponent,
+                 GraficoDonaComponent],
   exports: [
     DashboardComponent,
     ProgressComponent,
@@ -19,7 +29,8 @@ import { PagesRoutingModule } from './pages-routing.module';
   ],
   imports: [
     SharedModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    FormsModule
   ]
 })
 export class PagesModule { }
